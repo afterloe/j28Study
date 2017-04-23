fb024fa573d2c3c914822a9333936d5dd269824b
afterloe <605728727@qq.com>
2017-04-23 17:28:39 +0800

    添加注释

--- src/com/github/afterloe/Demo1.java
+++ src/com/github/afterloe/Demo1.java
@@ -8,18 +8,27 @@ import java.util.function.Predicate;
 import com.github.afterloe.domain.Apple;
 
 class AppleUtil {
-	
+
+	/**
+	 * 假设你有一个Apple类，它有一个getColor方法，还有一个变量inventory保存着一个Apples的列表。你可能想要选出所
+	 * 有的绿苹果，并返回一个列表。通常我们用筛选（filter）一词来表达这个概念。把条件代码作为参数传递进去，这样可以避免filter方法
+	 * 出现重复的代码。
+	 */
 	public static boolean isGreeApple(Apple apple) {
 		return "gree".equals(apple.getColor());
 	}
-	
+
 	public static boolean isHeavyApple(Apple apple) {
 		return 150 < apple.getWeight();
 	}
-	
+
+	/**
+	 * 方 法 作 为 Predicate参数p传递进去
+	 */
 	public static Collection<Apple> filterApple(Collection<Apple> inventory, Predicate<Apple> function) {
 		Collection<Apple> __ = new ArrayList<Apple>();
-		for (Apple apple: inventory) {
+		for (Apple apple : inventory) {
+			//  符 合 function 所 代表的条件
 			if (function.test(apple)) {
 				__.add(apple);
 			}
@@ -30,6 +39,9 @@ class AppleUtil {
 
 public class Demo1 {
 
+	/**
+	 * 测试
+	 */
 	public static void main(String[] args) {
 		Collection<Apple> __ = new HashSet<Apple>();
 		__.add(new Apple(132, "red"));
@@ -38,7 +50,7 @@ public class Demo1 {
 		__.add(new Apple(140, "gree"));
 		__.add(new Apple(152, "red"));
 		__.add(new Apple(133, "gree"));
-		
+
 		System.out.println(AppleUtil.filterApple(__, AppleUtil::isGreeApple));
 		System.out.println(AppleUtil.filterApple(__, AppleUtil::isHeavyApple));
 	}
