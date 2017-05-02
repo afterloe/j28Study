d126212cdf031e4694cc1e1dccf65c80fdbb8169
afterloe <605728727@qq.com>
2017-05-02 21:51:30 +0800

    分支 合并框架运用

--- src/com/github/afterloe/Demo16.java
+++ src/com/github/afterloe/Demo16.java
@@ -2,6 +2,7 @@ package com.github.afterloe;
 
 import org.junit.Test;
 
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 /**
@@ -14,6 +15,18 @@ public class Demo16 {
         long maxSum = Stream
                 .iterate(1l, i -> i + 1)
                 .limit(30000)
+                .parallel()
+                .reduce(0l, Long::sum);
+
+        System.out.println(maxSum);
+    }
+
+    /**
+     * 高效率并行流
+     */
+    @Test
+    public void test1() {
+        long maxSum = LongStream.rangeClosed(1, 10000000)
                 .parallel()
                 .reduce(0l, Long::sum);
 
---
+++ src/com/github/afterloe/Demo17.java
@@ -0,0 +1,65 @@
+package com.github.afterloe;
+
+import org.junit.Test;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
+
+/**
+ * Created by afterloe on 5/2/2017.
+ *
+ * 使用分支/合并框架并行求和
+ */
+public class Demo17 {
+
+    @Test
+    public void test() {
+        long[] numbers = LongStream.rangeClosed(1l, 100000).toArray();
+
+        ToRunning task = new ToRunning(numbers);
+        long sum = new ForkJoinPool().invoke(task);
+        System.out.println(sum);
+    }
+}
+
+class ToRunning extends RecursiveTask<Long> {
+
+    private final long[] numbers;
+    private final int start;
+    private final int end;
+
+    public static final long THRESHOLD = 10000;
+
+    public ToRunning(long[] numbers) {
+        this(numbers, 0, numbers.length);
+    }
+
+    private ToRunning(long[] numbers, int start, int end) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Long compute() {
+        int length = end -start;
+        if (length <= THRESHOLD) return computeSequentially();
+
+        ToRunning leftTask = new ToRunning(numbers, start, start + length / 2);
+        leftTask.fork();
+        ToRunning rightTask = new ToRunning(numbers, start + length /2 , end);
+        Long rightResult = rightTask.compute();
+        Long leftResult = leftTask.join();
+
+        return rightResult + leftResult;
+    }
+
+    private long computeSequentially() {
+        long sum = 0;
+        for (int i = start; i < end; i++) sum += numbers[i];
+
+        return sum;
+    }
+}
+
