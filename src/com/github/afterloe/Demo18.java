ae52de8b11aef85a60fa68ffb8c985d854c30c01
afterloe <605728727@qq.com>
2017-05-03 21:34:17 +0800

    3% 不出10次 第11次 成功率高达99.99%

---
+++ src/com/github/afterloe/Demo18.java
@@ -0,0 +1,26 @@
+package com.github.afterloe;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.IntStream;
+
+/**
+ * Created by afterloe on 5/3/2017.
+ */
+public class Demo18 {
+
+    @Test
+    public void test() {
+        BigDecimal x = new BigDecimal("0.97");
+        for (int i = 0; i < 11; i++) {
+            x = x.multiply(x);
+            System.out.println(x);
+        }
+
+        System.out.println(x);
+    }
+}
